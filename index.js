5afb991f95d8a848274e6fec9c297c4411c91539
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-03-29 17:42:08 -0300

    implementação da classe Funcionário

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import {Cliente} from "./Cliente.js";
-import { Conta } from "./Conta/Conta.js";
 import {ContaCorrente} from "./Conta/ContaCorrente.js";
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
 import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-
+// Clientes e Contas
 const cliente1 = new Cliente('Cleiber Ziviani', '302712290-55');
-
 const contaCorrente = new ContaCorrente(0, cliente1, 1001, '31624-0');
 contaCorrente.depositar(500);
 contaCorrente.sacar(100);
@@ -18,4 +19,14 @@ const contasalario = new ContaSalario(cliente1);
 contasalario.depositar(100);
 contasalario.sacar(10);
 
-console.log(contasalario)
+//Funcionários e login
+const diretor = new Diretor('Cleiber Ziviani', 10500, '301712453-68');
+diretor.cadastrarSenha('123456');
+const estaLogadoDiretor = SistemaAutenticacao.login(diretor, '123456');
+
+const gerente = new Gerente('Nalva Ziviani', 6000, '889104556-00');
+gerente.cadastrarSenha('abc123');
+const estaLogado = SistemaAutenticacao.login(gerente, 'ab123');
+
+console.log(estaLogado);
+
